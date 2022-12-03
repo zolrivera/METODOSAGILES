806cf53b81ced183cf9055aa20a0ad1c6438d733
Karen Gil Jauregui <kgil@crossnet.la>
2022-12-03 16:24:01 -0500

    Subir imagenes - Crear anuncio Paso 6

--- anfitrion_script.js
+++ anfitrion_script.js
@@ -2,6 +2,10 @@
 
 const $seleccionArchivos = document.querySelector("#seleccionArchivos"),
   $imagenPrevisualizacion = document.querySelector("#imagenPrevisualizacion");
+  $imagenPrevisualizacion2 = document.querySelector("#imagenPrevisualizacion2");
+  $imagenPrevisualizacion3 = document.querySelector("#imagenPrevisualizacion3");
+  $imagenPrevisualizacion4 = document.querySelector("#imagenPrevisualizacion4");
+  $imagenPrevisualizacion5 = document.querySelector("#imagenPrevisualizacion5");
 
 // Escuchar cuando cambie
 $seleccionArchivos.addEventListener("change", () => {
@@ -10,6 +14,10 @@ $seleccionArchivos.addEventListener("change", () => {
   // Si no hay archivos salimos de la función y quitamos la imagen
   if (!archivos || !archivos.length) {
     $imagenPrevisualizacion.src = "";
+    $imagenPrevisualizacion2.src = "";
+    $imagenPrevisualizacion3.src = "";
+    $imagenPrevisualizacion4.src = "";
+    $imagenPrevisualizacion5.src = "";
     return;
   }
   // Ahora tomamos el primer archivo, el cual vamos a previsualizar
@@ -18,4 +26,23 @@ $seleccionArchivos.addEventListener("change", () => {
   const objectURL = URL.createObjectURL(primerArchivo);
   // Y a la fuente de la imagen le ponemos el objectURL
   $imagenPrevisualizacion.src = objectURL;
+  if(objectURL.length>0){
+    $('#btnGuardar').prop('disabled', false)
+  }
+
+  const segundoArchivo = archivos[1];
+  const objectURL2 = URL.createObjectURL(segundoArchivo);
+  $imagenPrevisualizacion2.src = objectURL2;
+
+  const tercerArchivo = archivos[2];
+  const objectURL3 = URL.createObjectURL(tercerArchivo);
+  $imagenPrevisualizacion3.src = objectURL3;
+
+  const cuartoArchivo = archivos[3];
+  const objectURL4 = URL.createObjectURL(cuartoArchivo);
+  $imagenPrevisualizacion4.src = objectURL4;
+
+  const quintoArchivo = archivos[4];
+  const objectURL5 = URL.createObjectURL(quintoArchivo);
+  $imagenPrevisualizacion5.src = objectURL5;
 });
